06531e0d3f53f6302b270c677a5768662117c6ab
Neej from SevenTunes <69245976+seven-tunes@users.noreply.github.com>
2020-08-11 21:46:10 +0530

    Update en_us.ConfigManager.php

--- files/en_us.ConfigManager.php
+++ files/en_us.ConfigManager.php
@@ -3,5 +3,5 @@
 $mod_strings['LBL_CONFIG_MANAGER'] = 'Config Manager';
 $mod_strings['LBL_CONFIG_MANAGER_DESCRIPTION'] = 'Search or Update Runtime Configuration';
 
-$mod_strings['LBL_RUNTIME_CONFIGURATION_DESCRIPTION'] = 'Runtime Configuration';
+$mod_strings['LBL_RUNTIME_CONFIGURATION'] = 'Runtime Configuration';
 $mod_strings['LBL_RUNTIME_CONFIGURATION_DESCRIPTION'] = 'Manages Runtime Configuration';
